2f5a550dd37733a235388310a3d4cc27fd1b3158
cesarmatos2006 <matoscesar36@gmail.com>
2025-10-20 15:16:30 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,20 +14,3 @@ root.render(
 </React.StrictMode>
 
 );
- 
-import '@testing-library/jest-dom';
-
-import { render, screen } from '@testing-library/react';
-
-import App from './App';
- 
-test('renders title correctly', () => {
-
-  render(<App />);
-
-  const titleElement = screen.getByText(/React Demo/i);
-
-  expect(titleElement).toBeInTheDocument();
-
-});
- 
